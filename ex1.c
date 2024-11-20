de51d346a7d6a24493f8b007f2842a9fb0d2a93a
P3tel <oribahatpetel@gmail.com>
2024-11-20 22:03:27 +0200

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -12,19 +12,19 @@ int main()
   
     //ex 1
     printf("What bit:\n");
-    printf("Please enter a number\n");
+    printf("Please enter a number:\n");
     scanf("%d",&x); 
-    printf("Please enter a position\n");
+    printf("Please enter a position:\n");
     scanf("%d",&y);
     z = (x >> y) & 1;
-    printf("the bit in position %d of number %d is: %d",y,x,z );
+    printf("The bit in position %d of number %d is: %d",y,x,z );
     printf("\n\n");
   
     //ex 2
     printf("Set bit:\n");
-    printf("Please enter a number\n");
+    printf("Please enter a number:\n");
     scanf("%d",&x); 
-    printf("Please enter a position\n");
+    printf("Please enter a position:\n");
     scanf("%d",&y);
     z = x | (1 << y);
     n = x & ~(1 << y);
@@ -34,9 +34,9 @@ int main()
   
     //ex 3
     printf("Toggle bit:\n");
-    printf("Please enter a number\n");
+    printf("Please enter a number:\n");
     scanf("%d",&x); 
-    printf("Please enter a position\n");
+    printf("Please enter a position:\n");
     scanf("%d",&y);
     z = x ^ (1 << y);
     printf("Number with bit %d toggled: %d",y,z);
@@ -44,7 +44,7 @@ int main()
   
     //ex 4
     printf("Even - Odd:\n");
-    printf("Please enter a number\n");
+    printf("Please enter a number:\n");
     scanf("%d",&x); 
     y = (x & 1) ^ 1;
     printf("%d",y);
@@ -61,7 +61,7 @@ int main()
     b5 = 1 & (z >> 5);
     b7 = 1 & (z >> 7);
     b11 = 1 & (z >> 11);
-    printf("The sum in hexadecimal is: %X\n",z);
+    printf("The sum in hexadecimal: %X\n",z);
     printf("The 3,5,7,11 bits are: %d%d%d%d",b3,b5,b7,b11);
     printf("\nBye!");
     return 0;
